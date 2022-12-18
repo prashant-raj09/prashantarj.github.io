1984c7f677ced0e9164707806fd4f55fc70011a5
prashant-raj09 <95146202+prashant-raj09@users.noreply.github.com>
2022-12-18 23:47:17 +0530

    Add files via upload

---
+++ sidv.github.io-main/script.js
@@ -0,0 +1,7 @@
+// ============ typing animation=========
+var typed = new Typed(".typing",{
+    strings:["","Software Devloper","Frontend Developer","Competetive Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
